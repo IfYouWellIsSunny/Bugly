d0e9f2c4ee408b0147cc0ac85d1d63d6f1b21421
LeeJ <1285370698@qq.com>
2018-02-28 09:06:03 +0800

    Initial commit

---
+++ app/src/main/java/com/lj/bugly/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lj.bugly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tencent.bugly.Bugly;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bugly.init(getApplicationContext(), "f23cd6d032", false);
+
+    }
+}
